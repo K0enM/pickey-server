a0afd39f23f96189a72c3d278167ef2909fce6b7
Koen Molenaar <koen.molenaar26@gmail.com>
2021-03-17 13:13:14 +0100

    Eerste migration

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
 import os
 from flask import Flask
 from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 load_dotenv()
 
 app = Flask(__name__)
 app.config.from_object(os.getenv('APP_SETTINGS'))
-print(os.getenv('APP_SETTINGS'))
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class Hanger(db.Model):
+  __tablename__ = 'hangers'
+
+  id = db.Column(db.Integer, primary_key=True)
+  weight = db.Column(db.Float)
+
+  def __init__(self, weight):
+    self.weight = weight
+
+  def __repr__(self):
+    return '<id {}>'.format(self.id)
 
 @app.route('/')
 def hello():
--- config.py
+++ config.py
@@ -1,11 +1,14 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
   DEBUG = False
   TESTING = False
   CSRF_ENABLED = True
-  SECRET_KEY = 'dsdaerquwodjvhzklcvdusfoafjda'
+  SECRET_KEY = os.getenv("SECRET_KEY")
+  SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
 
 class DevelopmentConfig(Config):
   DEVELOPMENT = True
